9a9266995dfc29cc324952d6be870e9514a8e716
Dendup67 <dendup1998@gmail.com>
2023-08-29 10:04:15 +0600

    GCOA report

--- erpnext/accounts/doctype/consolidation_transaction/consolidation_transaction.js
+++ erpnext/accounts/doctype/consolidation_transaction/consolidation_transaction.js
@@ -3,20 +3,22 @@
 
 frappe.ui.form.on('Consolidation Transaction', {
 	refresh: function(frm) {
-		frm.add_custom_button(__("Make Adjustment Entry"),
-		()=>{
-			console.log(frm.doc.name)
-			frappe.model.open_mapped_doc({
-			method: "erpnext.accounts.doctype.consolidation_transaction.consolidation_transaction.make_adjustment_entry",
-			frm: frm
-		});
-		})
+		if (frm.doc.docstatus == 1) {
+			frm.add_custom_button(__("Make Adjustment Entry"),
+			()=>{
+				console.log(frm.doc.name)
+				frappe.model.open_mapped_doc({
+				method: "erpnext.accounts.doctype.consolidation_transaction.consolidation_transaction.make_adjustment_entry",
+				frm: frm
+				});
+			})
+				frm.add_custom_button(__('Consolidation Report'), function () {
+					frappe.route_options = {
+						"from_date": frm.doc.from_date,
+						"to_date": frm.doc.to_date
+					};
+					frappe.set_route("query-report", "Consolidation Report");
+				}, "fa fa-table");
+			}
 	},
-	make_adjustment_entry:function(frm){
-		console.log('here')
-		// frappe.model.open_mapped_doc({
-		// 	method: "erpnext.projects.doctype.project.project.make_project_advance",
-		// 	frm: frm
-		// });
-	}
 });
