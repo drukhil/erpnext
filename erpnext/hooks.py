eaf8b0676d2decbd435405a601b648f1b0e1cbd8
jai <jai>
2023-09-06 23:24:39 -0400

    disable pay_slip print and hooks replace jigme email

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -8,11 +8,11 @@ app_publisher = "Frappe Technologies Pvt. Ltd."
 app_description = """ERP made simple"""
 app_icon = "icon-th"
 app_color = "#e74c3c"
-app_email = "jigme@gyalsunginfra.bt"
+app_email = "jairajrai@gyalsunginfra.bt"
 app_license = "GNU General Public License (v3)"
 source_link = "https://github.com/frappe/erpnext"
 
-error_report_email = "jigme@gyalsunginfra.bt"
+error_report_email = "jairajrai@gyalsunginfra.bt"
 
 app_include_js = "assets/js/erpnext.min.js"
 app_include_css = "assets/css/erpnext.css"
