f9e23136becc5417a2fab9e80a21bb2ae5dbbc54
jai <jai>
2023-09-10 08:31:37 -0400

    cc on pull only non group

--- erpnext/hr/doctype/dfg/dfg.js
+++ erpnext/hr/doctype/dfg/dfg.js
@@ -25,7 +25,8 @@ frappe.ui.form.on('DFG', {
 						args: {
 								doctype: 'Cost Center',
 								filters: {
-										'branch': frm.doc.branch
+										'branch': frm.doc.branch,
+										'is_group': 0
 								},
 								fieldname: ['name']
 						},
