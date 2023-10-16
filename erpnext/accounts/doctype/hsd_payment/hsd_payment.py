fbd2fec9c918fb338bfd359697fa74f161043e9d
jai <jai>
2023-10-16 07:45:09 -0400

    hsd payment py again again

--- erpnext/accounts/doctype/hsd_payment/hsd_payment.py
+++ erpnext/accounts/doctype/hsd_payment/hsd_payment.py
@@ -71,7 +71,7 @@ class HSDPayment(Document):
 					doc.db_set("paid_amount", flt(doc.paid_amount) - flt(a.allocated_amount))
 					doc.db_set("outstanding_amount", flt(doc.outstanding_amount) + flt(a.allocated_amount))	
 				else:
-					paid_amount = flt(doc.paid_amount) + flt(a.allocated_amount)
+					paid_amount = round(flt(doc.paid_amount) + flt(a.allocated_amount), 2)
 					if flt(paid_amount, 2) > flt(doc.total_amount, 2):
 						frappe.throw("Paid Amount cannot be greater than the Total Amount for Receive POl <b>"+str(a.pol)+"</b>")
 					doc.db_set("paid_amount", paid_amount)
