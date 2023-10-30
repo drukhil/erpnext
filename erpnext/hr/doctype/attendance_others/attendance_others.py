8aae363c704f780ffc2eab3adf868be42153dc4a
jai <jai>
2023-10-30 02:41:04 -0400

    comment out the validator in Attendance Others

--- erpnext/hr/doctype/attendance_others/attendance_others.py
+++ erpnext/hr/doctype/attendance_others/attendance_others.py
@@ -8,11 +8,12 @@ from frappe.model.document import Document
 
 class AttendanceOthers(Document):
 	def validate(self):
-                res = frappe.db.sql("""select name from `tabAttendance Others` where employee = %s and date = %s
-                        and name != %s and docstatus = 1""",
-                        (self.employee, self.date, self.name))
-                if res:
-                        frappe.throw(("Attendance for employee {0} is already marked").format(self.employee))
+                pass
+                # res = frappe.db.sql("""select name from `tabAttendance Others` where employee = %s and date = %s
+                #         and name != %s and docstatus = 1""",
+                #         (self.employee, self.date, self.name))
+                # if res:
+                #         frappe.throw(("Attendance for employee {0} is already marked").format(self.employee))
 
 	def on_cancel(self):
                 user = frappe.session.user
