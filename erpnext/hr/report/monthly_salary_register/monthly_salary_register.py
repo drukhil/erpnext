fc9a61c80a6019750bf865b02b83ee4054af9c6a
kinleydorji <kd77144@gmail.com>
2023-11-24 15:15:06 +0600

    monthly salary register

--- erpnext/hr/report/monthly_salary_register/monthly_salary_register.py
+++ erpnext/hr/report/monthly_salary_register/monthly_salary_register.py
@@ -90,7 +90,7 @@ def get_columns(salary_slips, filters):
                     _("Branch") + ":Link/Branch:120", _("Department") + ":Link/Department:120", _("Division") + ":Link/Division:120",
                     _("Grade") + ":Link/Employee Grade:120", _("Designation") + ":Link/Designation:120",
             _("Year") + "::80", _("Month") + "::80", _("Leave Without Pay") + ":Float:130", 
-            _("Payment Days") + ":Float:120", _("Status") + "::100"
+         _("Status") + "::100"
         ]
     else:
         columns = [
