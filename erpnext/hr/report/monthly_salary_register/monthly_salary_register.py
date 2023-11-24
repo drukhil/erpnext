0e64623197db4e123bb80a9e70b652b509862d21
kinleydorji <kd77144@gmail.com>
2023-11-24 15:32:30 +0600

    monthly salary register

--- erpnext/hr/report/monthly_salary_register/monthly_salary_register.py
+++ erpnext/hr/report/monthly_salary_register/monthly_salary_register.py
@@ -48,16 +48,6 @@ def execute(filters=None):
                 ss.fiscal_year, ss.month, ss.leave_withut_pay, ss.payment_days,
                             status]
         else:
-    columns = [
-        _("Employee") + ":Link/Employee:80", _("Employee Name") + "::140", _("Employment Type") + ":Link/Employment Type:120",
-        _("CID No") + "::120", _("Joining Date") + ":Date:100", _("Bank Name")+ "::80", _("Bank A/C#")+"::100", 
-        #_("Company") + ":Link/Company:120",
-        _("Cost Center") + ":Link/Cost Center:120",
-                _("Branch") + ":Link/Branch:120", _("Department") + ":Link/Department:120", _("Division") + ":Link/Division:120",_("Section") + ":Link/Section:120",
-                _("Grade") + ":Link/Employee Grade:120", _("Designation") + ":Link/Designation:120",
-        _("Year") + "::80", _("Month") + "::80", _("Leave Without Pay") + ":Float:130", _("Payment Days") + "::50",  
-        _("Status") + "::100"
-    ]
             row = [
                 ss.employee,ss.employee_name, ss.employment_type, cid, joining_date,
                 ss.bank_name, ss.bank_ac_no, 
