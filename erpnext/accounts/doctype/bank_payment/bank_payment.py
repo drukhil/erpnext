2f89993993404f5d5548e7409e7d4fa2d32d31e4
Dendup67 <dendup1998@gmail.com>
2023-12-21 12:57:52 +0600

    PI number generation

--- erpnext/accounts/doctype/bank_payment/bank_payment.py
+++ erpnext/accounts/doctype/bank_payment/bank_payment.py
@@ -828,15 +828,12 @@ class BankPayment(Document):
 			bank_payment = self.name,
 			branch = self.branch,
 			cond = cond), as_dict=True)
-	# HSD payment 
-	def get_hsd_payment(self):
-     cond = ""
-     if self.transactoin_no:
-         cond = 'and hsd.name ="{}"'
+	# # HSD payment 
+	# def get_hsd_payment(self):
+	#  cond = ""
+	#  if self.transactoin_no:
+	# 	 cond = 'and hsd.name ="{}"'
 
-
-     
-     
 	#added by cety on 12/8/2021 to make payment for imprest recoup
 	def get_imprest_recoup_payment(self):
 		cond = ""
@@ -1181,11 +1178,11 @@ def upload_files(doc):
 	sftp.close()
 		
 def get_transaction_id(bank="BOBL"):
-    promo_code, company_code = frappe.db.get_value('Bank Payment Settings', bank, ['promo_code','company_specific_code'])
-    # return make_autoname(str(promo_code) + '.YYYY.MM.DD.##' + str(company_code) + '###')
-    formatted_company_code = '{:05d}'.format(int(company_code))
-    name_format = "{}.YYYY.MM.DD.{}.###".format(promo_code, formatted_company_code)
-    return make_autoname(name_format)
+	promo_code, company_code = frappe.db.get_value('Bank Payment Settings', bank, ['promo_code','company_specific_code'])
+	# return make_autoname(str(promo_code) + '.YYYY.MM.DD.##' + str(company_code) + '###')
+	formatted_company_code = '{:05d}'.format(int(company_code))
+	name_format = "{}.YYYY.MM.DD.{}.###".format(promo_code, formatted_company_code)
+	return make_autoname(name_format)
 
 def get_filename(note_type, posting_date, pi_number):
 	posting_date = posting_date.strftime('%Y%m%d%H%M%S')
