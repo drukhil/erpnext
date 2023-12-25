2ba22df0305b21f99e1c379f711f1c77fec0ef87
Gyeltshenwang <khotshawangdi@gmail.com>
2023-12-25 15:58:10 +0600

    made some changes

--- erpnext/accounts/doctype/bank_payment/bank_payment.py
+++ erpnext/accounts/doctype/bank_payment/bank_payment.py
@@ -383,10 +383,10 @@ class BankPayment(Document):
 	def get_hsd_payment(self):
 		cond = ""
 		if self.transaction_no:
-			cond = "  AND pe.name = '{}'".format(self.transaction_no)
+			cond = "  and pe.name = '{}'".format(self.transaction_no)
 	
 		elif not self.transaction_no and self.from_date and self.to_date:
-				cond = 'AND pe.posting_date BETWEEN "{}" AND "{}"'.format(str(self.from_date), str(self.to_date))
+				cond = 'and pe.posting_date BETWEEN "{}" AND "{}"'.format(str(self.from_date), str(self.to_date))
 		
 		return frappe.db.sql("""SELECT
                        "HSD Payment" transaction_type,
