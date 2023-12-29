9755ccf55c726d4a31d8e3e4f6de48994d92a337
kinleydorji <kd77144@gmail.com>
2023-12-29 18:16:09 +0600

    bulk leave encashment bank payment

--- erpnext/accounts/doctype/bank_payment/bank_payment.py
+++ erpnext/accounts/doctype/bank_payment/bank_payment.py
@@ -381,6 +381,8 @@ class BankPayment(Document):
 			data = self.get_loan_detail()
 		elif self.transaction_type == "HSD Payment":
 			data = self.get_hsd_payment()
+		elif self.transaction_type == "Bulk Leave Encashment":
+			data = self.get_leave_encashment()
 		return data
 
 	def get_salary_arrear(self):
@@ -604,10 +606,10 @@ class BankPayment(Document):
 						if reference_type and reference_name:
 							if reference_type in ['Travel Authorization','Travel Claim','Overtime Application','Leave Encashment','Employee Benefits', 'Salary Advance']:
 								party_type = "Employee"
-								party      = frappe.db.get_value(reference_type, reference_name, "employee")
+								party	  = frappe.db.get_value(reference_type, reference_name, "employee")
 							else:
 								party_type = "Supplier"
-								party      = frappe.db.get_value(reference_type, reference_name, "supplier")
+								party	  = frappe.db.get_value(reference_type, reference_name, "supplier")
 						if not party:
 							frappe.msgprint("Party missing for Journal Entry {}".format(a.transaction_id))
 				if party and party_type:
@@ -941,6 +943,46 @@ class BankPayment(Document):
 		""".format(bank_payment = self.name,
 					branch = self.branch,
 					cond = cond), as_dict=True)
+
+    def get_leave_encashment(self):
+        cond = ""
+        if not self.fiscal_year:
+            frappe.throw(_("Please select Fiscal Year"))
+        elif not self.month:
+            frappe.throw(_("Please select Month"))
+
+        cond = self.get_conditions()
+        return frappe.db.sql("""SELECT "Bulk Leave Encashment" transaction_type, t1.name transaction_id, 
+                        t1.name transaction_reference, t1.modified transaction_date,
+                        t2.employee, t2.employee_name beneficiary_name, 
+                        e.bank_name bank_name, 
+                        e.bank_branch bank_branch, fib.financial_system_code,
+                        e.bank_account_type,
+                        IFNULL(t2.bank_ac_no, e.bank_ac_no) bank_account_no, 
+                        round(t2.payable_amount,2) amount,
+                        'Bulk Leave Encashment for {salary_year}' remarks, "Draft" status						
+                    FROM `tabBulk Leave Encashment` t1 JOIN `tabBulk Leave Encashment Item` t2
+                        ON t1.name = t2.parent
+                        JOIN `tabEmployee` e ON t2.employee = e.name
+                        LEFT JOIN `tabFinancial Institution Branch` fib ON fib.name =  e.bank_branch
+                    WHERE t1.fiscal_year = '{salary_year}'
+                    AND t1.docstatus = 1
+                    AND e.salary_mode = 'Bank'
+                    {cond}
+                    AND IFNULL(t2.payable_amount,0) > 0
+                    AND NOT EXISTS(select 1
+                        FROM `tabBank Payment Item` bpi, `tabBank Payment` bp
+                        WHERE bpi.transaction_type = 'Salary Slip'
+                        AND bp.name=bpi.parent
+                        AND bp.transaction_type not in ('Employee Loan Payment')
+                        AND bpi.transaction_id = t1.name
+                        AND bpi.parent != '{bank_payment}'
+                        AND bpi.docstatus != 2
+                        AND bpi.status NOT IN ('Cancelled', 'Failed')
+                    )
+        """.format(salary_year=self.fiscal_year, 
+            bank_payment = self.name,
+            cond = cond), as_dict=True)
   
 def process_one_to_one_payment(doc, publish_progress=True):
 	stat = 0
@@ -1249,12 +1291,12 @@ def get_inr_bank_file(doc, filename, posting_date):
 	# now  = datetime.datetime.now()
 	# transaction_type = '01' if rec else '51'
 	# bank_account_no  = rec.bank_account_no if rec else account.bank_account_no
-	# amount           = int(flt(rec.amount,2)*100) if rec else int(flt(total,2)*100)
-	# narration        = self.name
-	# narration       += "|"+str(rec.transaction_id)+"|"+str(rec.beneficiary_name) if rec else "|"+self.name+"|NRDCL"
-	# narration       += "|"+str(self.branch)
-	# branch_no        = '20'
-	# value_date       = now.strftime('%d%m%Y')
+	# amount		   = int(flt(rec.amount,2)*100) if rec else int(flt(total,2)*100)
+	# narration		= self.name
+	# narration	   += "|"+str(rec.transaction_id)+"|"+str(rec.beneficiary_name) if rec else "|"+self.name+"|NRDCL"
+	# narration	   += "|"+str(self.branch)
+	# branch_no		= '20'
+	# value_date	   = now.strftime('%d%m%Y')
 
 	# formatted = (str(transaction_type)[:2]
 	# 		+ str(bank_account_no).rjust(17,str('0'))[:17]
