8de354be3be26699ebb91771192a0b20320395b5
kinleydorji <kd77144@gmail.com>
2023-12-29 19:54:10 +0600

    journal entry account bulk leave encashment

--- erpnext/accounts/doctype/bank_payment/bank_payment.py
+++ erpnext/accounts/doctype/bank_payment/bank_payment.py
@@ -944,45 +944,45 @@ class BankPayment(Document):
 					branch = self.branch,
 					cond = cond), as_dict=True)
 
-    def get_leave_encashment(self):
-        cond = ""
-        if not self.fiscal_year:
-            frappe.throw(_("Please select Fiscal Year"))
-        elif not self.month:
-            frappe.throw(_("Please select Month"))
+	def get_leave_encashment(self):
+		cond = ""
+		if not self.fiscal_year:
+			frappe.throw(_("Please select Fiscal Year"))
+		elif not self.month:
+			frappe.throw(_("Please select Month"))
 
-        cond = self.get_conditions()
-        return frappe.db.sql("""SELECT "Bulk Leave Encashment" transaction_type, t1.name transaction_id, 
-                        t1.name transaction_reference, t1.modified transaction_date,
-                        t2.employee, t2.employee_name beneficiary_name, 
-                        e.bank_name bank_name, 
-                        e.bank_branch bank_branch, fib.financial_system_code,
-                        e.bank_account_type,
-                        IFNULL(t2.bank_ac_no, e.bank_ac_no) bank_account_no, 
-                        round(t2.payable_amount,2) amount,
-                        'Bulk Leave Encashment for {salary_year}' remarks, "Draft" status						
-                    FROM `tabBulk Leave Encashment` t1 JOIN `tabBulk Leave Encashment Item` t2
-                        ON t1.name = t2.parent
-                        JOIN `tabEmployee` e ON t2.employee = e.name
-                        LEFT JOIN `tabFinancial Institution Branch` fib ON fib.name =  e.bank_branch
-                    WHERE t1.fiscal_year = '{salary_year}'
-                    AND t1.docstatus = 1
-                    AND e.salary_mode = 'Bank'
-                    {cond}
-                    AND IFNULL(t2.payable_amount,0) > 0
-                    AND NOT EXISTS(select 1
-                        FROM `tabBank Payment Item` bpi, `tabBank Payment` bp
-                        WHERE bpi.transaction_type = 'Salary Slip'
-                        AND bp.name=bpi.parent
-                        AND bp.transaction_type not in ('Employee Loan Payment')
-                        AND bpi.transaction_id = t1.name
-                        AND bpi.parent != '{bank_payment}'
-                        AND bpi.docstatus != 2
-                        AND bpi.status NOT IN ('Cancelled', 'Failed')
-                    )
-        """.format(salary_year=self.fiscal_year, 
-            bank_payment = self.name,
-            cond = cond), as_dict=True)
+		cond = self.get_conditions()
+		return frappe.db.sql("""SELECT "Bulk Leave Encashment" transaction_type, t1.name transaction_id, 
+						t1.name transaction_reference, t1.modified transaction_date,
+						t2.employee, t2.employee_name beneficiary_name, 
+						e.bank_name bank_name, 
+						e.bank_branch bank_branch, fib.financial_system_code,
+						e.bank_account_type,
+						IFNULL(t2.bank_ac_no, e.bank_ac_no) bank_account_no, 
+						round(t2.payable_amount,2) amount,
+						'Bulk Leave Encashment for {salary_year}' remarks, "Draft" status						
+					FROM `tabBulk Leave Encashment` t1 JOIN `tabBulk Leave Encashment Item` t2
+						ON t1.name = t2.parent
+						JOIN `tabEmployee` e ON t2.employee = e.name
+						LEFT JOIN `tabFinancial Institution Branch` fib ON fib.name =  e.bank_branch
+					WHERE t1.fiscal_year = '{salary_year}'
+					AND t1.docstatus = 1
+					AND e.salary_mode = 'Bank'
+					{cond}
+					AND IFNULL(t2.payable_amount,0) > 0
+					AND NOT EXISTS(select 1
+						FROM `tabBank Payment Item` bpi, `tabBank Payment` bp
+						WHERE bpi.transaction_type = 'Salary Slip'
+						AND bp.name=bpi.parent
+						AND bp.transaction_type not in ('Employee Loan Payment')
+						AND bpi.transaction_id = t1.name
+						AND bpi.parent != '{bank_payment}'
+						AND bpi.docstatus != 2
+						AND bpi.status NOT IN ('Cancelled', 'Failed')
+					)
+		""".format(salary_year=self.fiscal_year, 
+			bank_payment = self.name,
+			cond = cond), as_dict=True)
   
 def process_one_to_one_payment(doc, publish_progress=True):
 	stat = 0
