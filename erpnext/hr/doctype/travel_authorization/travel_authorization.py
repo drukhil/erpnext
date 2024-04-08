91448bc31465747ae8a71ee31c7aa3d1b1b3bb02
jai <jai>
2024-04-08 05:56:50 -0400

    update in create_attendance in Travel Authorization

--- erpnext/hr/doctype/travel_authorization/travel_authorization.py
+++ erpnext/hr/doctype/travel_authorization/travel_authorization.py
@@ -120,7 +120,7 @@ class TravelAuthorization(Document):
 		days = date_diff(e, d) + 1
 		for a in (d + timedelta(n) for n in range(days)):
 			al = frappe.db.sql("select name from tabAttendance where docstatus = 1 and employee = %s and att_date = %s", (self.employee, a), as_dict=True)
-			if al:
+			if len(al):
 				doc = frappe.get_doc("Attendance", al[0].name)
 				doc.cancel()
 			#create attendance
