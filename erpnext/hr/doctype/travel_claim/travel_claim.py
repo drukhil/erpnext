9c3d6e700fee3038517d8976b2a40de7fe758cfd
jai <jai>
2024-05-09 06:58:23 -0400

    travel claim verifier send mail condition added

--- erpnext/hr/doctype/travel_claim/travel_claim.py
+++ erpnext/hr/doctype/travel_claim/travel_claim.py
@@ -50,7 +50,8 @@ class TravelClaim(Document):
 		
 		if frappe.session.user == self.owner or frappe.session.user == employee:
 			self.db_set("claim_status", "")
-			self.sendmail(frappe.db.get_value("Employee", {"user_id": self.supervisor}, "name"), "Travel Claim Submitted", str(self.employee_name) + " has requested you to verify and sign a " + str(frappe.get_desk_link("Travel Claim", self.name)))
+			if self.workflow_state == "Waiting Approval":
+				self.sendmail(frappe.db.get_value("Employee", {"user_id": self.supervisor}, "name"), "Travel Claim Submitted", str(self.employee_name) + " has requested you to verify and sign a " + str(frappe.get_desk_link("Travel Claim", self.name)))
 		elif self.claim_status == "Rejected by Supervisor":
 			self.sendmail(self.employee, "Travel Claim Rejected by Supervisor" + str(self.name), "Following remarks has been added by the supervisor: \n" + str(self.reason))
 		elif self.claim_status == "Rejected by HR":
